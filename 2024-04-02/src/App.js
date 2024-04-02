cb2fff652e2124d89d0d02e8a581eec87b26558c
Chloe Yeo <yeochloe01@gmail.com>
2024-04-02 12:03:41 +0900

    buttons onClick setState implemented

--- 2024-04-02/src/App.js
+++ 2024-04-02/src/App.js
@@ -2,16 +2,29 @@ import React, { useState } from "react";
 import "./assets/css/style.css";
 
 function App() {
-  const [test, setTest] = useState("chloe");
-  function testChange() {
-    alert("clicked");
-    setTest("Yeo");
+  const [test, setTest] = useState("initial");
+  function testChange(msg) {
+    setTest(msg);
   }
+  console.log("component re-renders since state changed");
   return (
     <>
       <h1>{test}</h1>
-      <button className="btn primary" onClick={testChange}>
-        Click
+      <button
+        className="btn primary"
+        onClick={() => {
+          testChange("yeo");
+        }}
+      >
+        Click A
+      </button>
+      <button
+        className="btn"
+        onClick={() => {
+          testChange("chloe");
+        }}
+      >
+        Click B
       </button>
     </>
   );
