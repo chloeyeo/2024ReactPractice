d1d195edf6ea473cd228b35b2fbb86f89aaf616b
Chloe Yeo <yeochloe01@gmail.com>
2024-04-03 11:19:23 +0900

    open and close modal react

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,10 +2,42 @@ import React, { useState } from "react";
 import "./assets/css/style.css";
 
 const App = () => {
-  const [num, setNum] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
   return (
     <>
-      <div className="wrap">parent component {num}</div>
+      <div>
+        <button
+          className="btn primary"
+          onClick={() => {
+            setOpenModal(!openModal);
+          }}
+        >
+          Open Modal
+        </button>
+      </div>
+      {openModal ? (
+        <Modal openModal={openModal} setOpenModal={setOpenModal} />
+      ) : null}
+    </>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <>
+      <div className="modalWrap">
+        <div className="modal">
+          Modal Window
+          <button
+            className="btn"
+            onClick={() => {
+              props.setOpenModal(!props.openModal);
+            }}
+          >
+            Close Modal
+          </button>
+        </div>
+      </div>
     </>
   );
 };
