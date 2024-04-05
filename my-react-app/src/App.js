271fcc4122b8afb9cb848f57a3e1b66532e24b7e
Chloe Yeo <yeochloe01@gmail.com>
2024-04-05 16:45:42 +0900

    pagination in react implemented

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -10,7 +10,8 @@ const App = () => {
   const [viewData, setViewData] = useState([]);
   const [totalPage, setTotalPage] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
-  const [listCnt, setListCnt] = useState(5);
+  const [listCnt] = useState(5);
+  const [paginationBulletCnt] = useState(3);
   useEffect(() => {
     // ? is a query string (?_page=&_limit=)
     axios
@@ -26,7 +27,35 @@ const App = () => {
       .catch((error) => {
         console.error(error);
       });
-  }, [currentPage]);
+  }, [currentPage, listCnt]);
+  // startPage = Math.floor((page-1)/pageCnt*pageCnt+1)
+  // endPage = startPage+pageCnt-1
+  const pageNumber = () => {
+    let middleBullet = Math.ceil(paginationBulletCnt / 2);
+    // let startPage = Math.min(
+    //   totalPage - paginationBulletCnt + 1,
+    //   Math.floor(
+    //     ((currentPage - 1) / paginationBulletCnt) * paginationBulletCnt + 1
+    //   )
+    // );
+    // let endPage = Math.min(startPage + paginationBulletCnt - 1, totalPage);
+    let startPage = Math.max(1, currentPage - middleBullet + 1);
+    let endPage = Math.min(totalPage, startPage + paginationBulletCnt - 1);
+    let paginationBullets = [];
+    for (let i = startPage; i <= endPage; i++) {
+      paginationBullets.push(
+        <li
+          onClick={() => {
+            setCurrentPage(i);
+          }}
+          className={i === currentPage ? "active" : null}
+        >
+          {i}
+        </li>
+      );
+    }
+    return paginationBullets;
+  };
   return (
     <>
       <h3>Total Pages: {totalPage}</h3>
@@ -41,64 +70,27 @@ const App = () => {
         })}
       </ul>
       <div className="pagination">
-        <ul>
-          <li
+        {currentPage > 1 ? (
+          <div
+            className="prev"
             onClick={() => {
-              setCurrentPage(currentPage - 1);
+              setCurrentPage(Math.max(1, currentPage - 1));
             }}
           >
             &#8678;
-          </li>
-          <li
+          </div>
+        ) : null}
+        <ul className="paginationBullets">{pageNumber()}</ul>
+        {currentPage !== totalPage ? (
+          <div
+            className="next"
             onClick={() => {
-              setCurrentPage(1);
-            }}
-          >
-            1
-          </li>
-          <li
-            onClick={() => {
-              setCurrentPage(2);
-            }}
-          >
-            2
-          </li>
-          <li
-            onClick={() => {
-              setCurrentPage(3);
-            }}
-          >
-            3
-          </li>
-          <li
-            onClick={() => {
-              setCurrentPage(4);
-            }}
-          >
-            4
-          </li>
-          <li
-            onClick={() => {
-              setCurrentPage(5);
-            }}
-          >
-            5
-          </li>
-          <li
-            onClick={() => {
-              setCurrentPage(6);
-            }}
-          >
-            6
-          </li>
-          <li
-            onClick={() => {
-              setCurrentPage(currentPage + 1);
+              setCurrentPage(Math.min(currentPage + 1, totalPage));
             }}
           >
             &#8680;
-          </li>
-        </ul>
+          </div>
+        ) : null}
       </div>
     </>
   );
