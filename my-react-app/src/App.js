a6e4d2a56121cd52b548ae1aba30ce71c9b94e2b
Chloe Yeo <yeochloe01@gmail.com>
2024-04-09 17:06:54 +0900

    update

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,7 +4,7 @@ import "./assets/css/tailwindStyle.css";
 const App = () => {
   return (
     <>
-      <div className="container mx-auto bg-gray-400 p-4 rounded-md text-white text-2xl hover:bg-gray-800">
+      <div className="container mx-auto bg-gray-400 p-4 rounded-md text-white text-2xl hover:bg-gray-800 shadow-lg mt-4">
         test
       </div>
     </>
