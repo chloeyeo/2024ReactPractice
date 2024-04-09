b21639e46b705e3b91c466584dc35920696554c4
Chloe Yeo <yeochloe01@gmail.com>
2024-04-09 16:50:47 +0900

    tailwind css color setup

--- my-react-app/tailwind.config.js
+++ my-react-app/tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*/.{js,jsx,ts,tsx}"],
+  //"./src/**/*.{js,jsx,ts,tsx}"
+  // "./src/*/.{js,jsx,ts,tsx}"
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     container: {
       screens: {
         sm: "640px",
-        md: "760px",
+        md: "768px",
         lg: "1024px",
         xl: "1280px",
       },
     },
+    // go to https://javisperez.github.io/tailwindcolorshades/
+    // to pick color for tailwind css
     extend: {
       colors: {
         sb: {
@@ -24,6 +28,19 @@ module.exports = {
           800: "#104070",
           900: "#092b54",
           950: "#041836",
+        },
+        newYorkPink: {
+          50: "#fcfaf7",
+          100: "#faf4f0",
+          200: "#f5e7df",
+          300: "#edd4ca",
+          400: "#deada4",
+          500: "#d28484",
+          600: "#bd6c6c",
+          700: "#9c4949",
+          800: "#7d2f2f",
+          900: "#5e1a1a",
+          950: "#3d0b0b",
         },
       },
     },
