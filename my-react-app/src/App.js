8d68f493b28b8c92e2fd00574fc4d6be5cf29c0e
Chloe Yeo <yeochloe01@gmail.com>
2024-04-09 17:09:36 +0900

    tailwind css className practice update

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import "./assets/css/tailwindStyle.css";
 
 const App = () => {
+  // gap-10 is gap 10px
   return (
     <>
-      <div className="container mx-auto bg-gray-400 p-4 rounded-md text-white text-2xl hover:bg-gray-800 shadow-lg mt-4">
-        test
+      <div className="container mx-auto bg-gray-400 p-4 rounded-md text-white grid grid-cols-3 gap-10">
+        {/* div*4>{test$} then tab */}
+        <div className="bg-sb-300 rounded-md p-4">test1</div>
+        <div className="bg-sb-300 rounded-md p-4">test2</div>
+        <div className="bg-sb-300 rounded-md p-4">test3</div>
+        <div className="bg-sb-300 rounded-md p-4">test4</div>
       </div>
     </>
   );
