79292fa8a0053df78b37ec149808473a5db78a40
Chloe Yeo <yeochloe01@gmail.com>
2024-04-11 11:24:41 +0900

    use react-redux, redux to manage state

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,58 +1,60 @@
 import React from "react";
 import "./assets/css/tailwindStyle.css";
+import { createStore } from "redux";
+import { Provider, useSelector } from "react-redux";
+
+// ctrl+shift+w + div.box
+
+function reducer(state, action) {
+  // state is the initialState we passed into store along with reducer
+  // console.log(state); // object -> num:0
+  return state;
+}
+
+const initialState = { num: 0 };
+
+const store = createStore(reducer, initialState);
 
 const App = () => {
-  // gap-10 is gap 10px
   return (
     <>
-      {/* <div className="container mx-auto bg-gray-400 p-4 rounded-md text-white grid grid-cols-1 md:grid-cols-3 gap-4"> */}
-      {/* div*4>{test$} then tab */}
-      {/* <div className="bg-sb-300 rounded-md p-4">test1</div>
-        <div className="bg-sb-300 rounded-md p-4">test2</div>
-        <div className="bg-sb-300 rounded-md p-4">test3</div>
-        <div className="bg-sb-300 rounded-md p-4 md:hidden">test4</div>
-      </div> */}
-      {/* flex-col is flex-direction:column; */}
-      <section className="flex max-w-[400px] mx-auto mt-20">
-        <div className="bg-white shadow-md rounded-md border p-6 w-full">
-          <h1 className="text-center text-2xl font-semibold mb-4">로그인</h1>
-          <hr className="mb-4" />
-          <div className="mb-6">
-            <label
-              htmlFor="email"
-              className="text-sm font-semibold text-gray-500 flex mb-2"
-            >
-              이메일
-            </label>
-            <input
-              type="text"
-              id="email"
-              className="w-full bg-white border rounded-md p-2 text-xs mb-2"
-              placeholder="이메일을 입력하세요"
-            />
-            <label
-              htmlFor="pwd"
-              className="text-sm font-semibold text-gray-500 flex mb-2"
-            >
-              패스워드
-            </label>
-            <input
-              type="text"
-              id="pwd"
-              className="w-full bg-white border rounded-md p-2 text-xs"
-              placeholder="패스워드를 입력하세요"
-            />
-          </div>
-          {/* <div className="mb-6"></div> */}
-          <div>
-            <button className="w-full text-xs text-white bg-gray-700 px-4 py-2 rounded-md hover:bg-gray-600">
-              회원가입
-            </button>
-          </div>
+      <Provider store={store}>
+        <div className="box border p-4">
+          <h3>title1</h3>
+          <h1>
+            <ChildOne />
+          </h1>
         </div>
-      </section>
+      </Provider>
     </>
   );
 };
+
+function ChildOne() {
+  return (
+    <>
+      <div className="box border p-4">
+        <h3>childone</h3>
+        <ChildTwo />
+      </div>
+    </>
+  );
+}
+
+function ChildTwo() {
+  // state is initialState passed into store
+  const num = useSelector((state) => {
+    return state.num;
+  });
+  // console.log(num);
+  return (
+    <>
+      <div className="box border p-4">
+        <h3>childtwo</h3>
+        number({num})
+      </div>
+    </>
+  );
+}
 
 export default App;
